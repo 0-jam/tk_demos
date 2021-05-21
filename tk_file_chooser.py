3a11920e3c01b9727839a7773dcdb2c145889296
0-jam <28038981+0-jam@users.noreply.github.com>
2021-05-21 15:02:03 +0900

    Added the label to show selected file name

--- tk_file_chooser.py
+++ tk_file_chooser.py
@@ -1,8 +1,9 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 import tkinter.filedialog as filedialog
 
 
-class MyApplication(tk.Frame):
+class TkFileChooser(ttk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
 
@@ -11,13 +12,21 @@ class MyApplication(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.file_chooser = tk.Button(self, text='open', command=filedialog.askopenfile)
+        self.file_chooser = ttk.Button(self, text='open', command=self.choose_file)
         self.file_chooser.pack(side='top')
 
-        self.quit_button = tk.Button(self, text='quit', fg='red', command=self.master.destroy)
+        self.file_name_label = ttk.Label(self, text='none')
+        self.file_name_label.pack()
+
+        self.quit_button = ttk.Button(self, text='quit', command=self.master.destroy)
         self.quit_button.pack(side='bottom')
+
+    def choose_file(self):
+        chosen_file = filedialog.askopenfile()
+
+        self.file_name_label['text'] = chosen_file.name
 
 
 root = tk.Tk()
-app = MyApplication(master=root)
+app = TkFileChooser(master=root)
 app.mainloop()
